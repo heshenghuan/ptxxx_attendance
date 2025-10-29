b83ad5b062115726d2570e24b30b3e559d6bc222
heshenghuan <heshenghuan@sina.com>
2025-10-29 21:07:33 +0800

    基于cookies进行pt站点的每日签到

---
+++ checkin.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+import re
+import yaml
+import requests
+import time
+
+_headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36 Edg/111.0.1661.41"
+}
+
+def login(url, cookies):
+    '''
+    获取登陆会话
+    '''
+    print('使用已有cookie登录')
+    cookies = requests.utils.cookiejar_from_dict(
+        {
+            'c_secure_pass': cookies
+        }
+    )
+    bt_session = requests.session()
+    bt_session.cookies = cookies
+    try:
+        torrent_url = f'https://{url}/torrents.php'
+        test = bt_session.get(torrent_url, headers=_headers)
+        if test.status_code != 200 or test.url != torrent_url:
+            print('测试获取torrents.php失败，疑似cookie失效，请重新登录')
+            return None
+    except Exception as e:
+        print("cookie失效，请重新登录:", e)
+        return None
+    return bt_session
+
+def checkin(session, url):
+    '''
+    签到
+    '''
+    attendance_php = f'https://{url}/attendance.php'
+    try:
+        checkin_page = session.get(attendance_php, headers=_headers)
+        
+        if checkin_page.status_code == 200 and ('签到成功' in checkin_page.text or '簽到成功' in checkin_page.text):
+            nowtime = time.strftime('%Y-%m-%d', time.localtime(time.time()))
+            # 使用正则获取签到天数和获得魔力值
+            signindays = re.search(r'已(?:连续签|連續簽)到 <b>(\d+)</b>', checkin_page.text, re.DOTALL).group(1)
+            integral = re.search(r'本次(?:签到获得|簽到獲得) <b>(\d+)</b> .魔力值', checkin_page.text, re.DOTALL).group(1)
+            signinrank = re.search(r'今日(?:签到|簽到)排名：(<b>\d+</b> / <b>\d+</b>)', checkin_page.text, re.DOTALL).group(1)
+            signinrank = signinrank.replace('<b>', '').replace('</b>', '')
+            print(nowtime + ' 签到成功    连续签到：' + signindays + '天    获得魔力值：' + integral + '    签到排名：' + signinrank)
+        else:
+            print("签到失败，可能是今日已签到或网络异常")
+    except Exception as e:
+        print("Attendance error:", e)
+
+def main():
+    with open("./config.yaml", "r", encoding="utf-8") as f:
+        cfg = yaml.load(f, Loader=yaml.FullLoader)
+    print('配置文件加载成功')
+    for station in cfg.keys():
+        url = cfg[station]['base_url']
+        cookies = cfg[station]['cookies']
+        print(f'正在处理 {station} 站点')
+        session = login(url, cookies)
+        if session:
+            print('登陆成功，正在签到...')
+            checkin(session, url)
+        else:
+            print(f'{station} 站点登录失败，跳过签到')
+        print('-----------------------------------')
+
+
+if __name__ == '__main__':
+    main()
